96bc05ca3bd9e57c09288d6612701e9c3ec1c5bc
Youss0203 <youssefhalibi003@gmail.com>
2023-11-20 12:16:27 +0100

    first commit

---
+++ cartella snack-4/js/script.js
@@ -0,0 +1,22 @@
+/*Crea una funzione che accetta due numeri, se il primo e' 
+divisibile per il secondo, allora ritorna vero, altrimenti ritorna falso. 
+Micro bonus: se sono uguali ritorna il numero.*/
+
+const UserFirstNumber = parseInt(prompt("type a number"))
+const UserSecondNumber = parseInt(prompt("type a number"))
+
+function isNumberDivisible(numberOne, numberTwo){
+if (numberOne % numberTwo) {
+    return true;
+} else { 
+    return false; 
+}
+}
+
+if(isNumberDivisible(UserFirstNumber,UserFirstNumber)){
+    console.log(UserFirstNumber,UserSecondNumber)
+}else if(UserFirstNumber % UserSecondNumber){
+    console.log(true)
+}else{
+    console.log(false)
+}
