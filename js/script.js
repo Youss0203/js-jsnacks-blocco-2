8675c5edcbbde922c135d64390b3908d8eda81de
Youss0203 <youssefhalibi003@gmail.com>
2023-11-23 12:21:43 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*Cliccando su un pulsante, viene avviato un cronometro. Per fermare il cronometro, 
+bisogna cliccare su un secondo pulsante*/
+
+
+btnStartElement=document.getElementById("button#start-button");
+
+btnStartElement.addEventListener("click", function(){
+    const clockCrono=setInterval(function(){
+        
+    })
+},1000)
